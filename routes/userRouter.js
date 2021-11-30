63114d187c499fb1d774c07ce837034e2655f17c
I3B <islamnaasani@gmail.com>
2021-11-30 15:20:21 +0200

    typo in user url

--- routes/userRouter.js
+++ routes/userRouter.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const router = express.Router();
 
-router.get("/user", (req, res) => {
+router.get("/", (req, res) => {
     if (req.user)
         res.json({ msg: "user is logged in", username: req.user.username });
     else res.json({ msg: "user is not logged in...", username: null });
