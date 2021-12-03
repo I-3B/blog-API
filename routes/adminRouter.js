c84382cdbda352ed56f3e57ca4c3bde5934d6f38
I3B <islamnaasani@gmail.com>
2021-12-03 18:56:57 +0200

    add admin route

--- routes/adminRouter.js
+++ routes/adminRouter.js
@@ -1,6 +1,33 @@
 const express = require("express");
+const passport = require("passport");
 const router = express.Router();
-router.get("/", (req, res) => {
-    res.send(req.user);
-});
+const adminController = require("../controllers/adminController");
+router.get(
+    "/",
+    passport.authenticate("jwt", { session: false }),
+    adminController.isUserAnAdmin
+);
+router.get("/posts", adminController.getAllPosts);
+router.get(
+    "/posts/:id",
+    passport.authenticate("jwt", { session: false }),
+    adminController.getPost
+);
+router.post(
+    "/posts",
+    passport.authenticate("jwt", { session: false }),
+    adminController.postPost
+);
+
+router.put(
+    "/posts/:id",
+    passport.authenticate("jwt", { session: false }),
+    adminController.editPost
+);
+router.delete(
+    "/posts/:id",
+    passport.authenticate("jwt", { session: false }),
+    adminController.deletePost
+);
+
 module.exports = router;
