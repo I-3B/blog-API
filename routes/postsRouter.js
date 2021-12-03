211e00ed7639ae45a0818c6ab0fb99e6b34262f0
I3B <islamnaasani@gmail.com>
2021-12-03 18:57:26 +0200

    remove related admin routes to admin route

--- routes/postsRouter.js
+++ routes/postsRouter.js
@@ -5,23 +5,7 @@ const postController = require("../controllers/postController");
 const commentController = require("../controllers/commentController");
 
 router.get("/", postController.getAllPosts);
-router.post(
-    "/",
-    passport.authenticate("jwt", { session: false }),
-    postController.postPost
-);
 router.get("/:id", postController.getPost);
-router.put(
-    "/:id",
-    passport.authenticate("jwt", { session: false }),
-    postController.editPost
-);
-router.delete(
-    "/:id",
-    passport.authenticate("jwt", { session: false }),
-    postController.deletePost
-);
-
 router.get("/:postId/comments", commentController.getAllComments);
 router.get("/:postId/comments/count", commentController.getAllCommentsCount);
 
